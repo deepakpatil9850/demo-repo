5e1c8b6baf641c0f9e9874077b3b43be4bb83609
Deepak Patil <deepakpatil0999@gmail.com>
2022-08-31 22:35:25 +0530

    dom append child demo

---
+++ jsPractice/DOMpractice1.js
@@ -0,0 +1,7 @@
+window.onload=()=>{
+    const heading=document.createElement("h1");
+    const headText=document.createTextNode("Hello World");
+
+    heading.appendChild(headText);
+    document.body.appendChild(heading);
+};
